d01c7af5fb44ad3a727cf5bfff9a9a293b61f448
cdzdn <floyd9732@gmail.com>
2020-10-15 09:57:57 -0300

    SQL Script to populate database added

---
+++ PopulateDatabase/Populate.sql
@@ -0,0 +1,113 @@
+﻿/* LANCAMENTOS */
+
+/* Janeiro */
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1024,354.30,'20200109 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1020,2.30,'20200113 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1024,13.30,'20200114 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1021,34.30,'20200122 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1022,90.30,'20200125 6:00:00 AM');
+
+/* Fevereiro */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1020,9.30,'20200209 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1024,12.30,'20200213 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1021,55.30,'20200214 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1023,60.30,'20200222 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1022,10,'20200225 6:00:00 AM');
+
+/* Marco */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1022,23.3,'20200309 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1021,98.2,'20200313 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1020,101.12,'20200314 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1023,1002.2,'20200322 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1024,540.23,'20200325 6:00:00 AM');
+
+/* Abril */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1020,619.10,'20200409 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1022,780.10,'20200413 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1024,900.99,'20200414 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1023,30.99,'20200422 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1022,12.50,'20200425 6:00:00 AM');
+
+/* Maio */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1024,93.23,'20200509 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1023,50.00,'20200513 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1024,20.00,'20200514 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1020,90.00,'20200522 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1022,110.00,'20200525 6:00:00 AM');
+
+/* Junho */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1021,5400.10,'20200609 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1020,1220.50,'20200613 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1021,33.20,'20200614 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1022,68.90,'20200622 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1020,50.25,'20200625 6:00:00 AM');
+
+/* Julho */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1021,125.30,'20200709 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1021,356.01,'20200713 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1020,566.60,'20200714 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1021,700.20,'20200722 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1022,840.20,'20200725 6:00:00 AM');
+
+/* Agosto */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1021,120.80,'20200809 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1020,122.90,'20200813 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1023,1.50,'20200814 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1022,1.25,'20200822 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1020,2.00,'20200825 6:00:00 AM');
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1020,120.80,'20200809 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1020,122.90,'20200810 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1020,1.50,'20200811 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1020,1.25,'20200812 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1020,2.00,'20200813 6:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1020,120.80,'20200814 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1020,122.90,'20200815 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1020,1.50,'20200816 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1020,1.25,'20200817 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1020,2.00,'20200818 6:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1020,120.80,'20200819 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1020,122.90,'20200820 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1020,1.50,'20200821 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1020,1.25,'20200822 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1020,2.00,'20200823 6:00:00 AM');
+
+/* Setembro */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1020,3.40,'20200909 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1024,25.00,'20200913 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1023,25.60,'20200914 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1022,12.50,'20200922 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1020,99.99,'20200925 6:00:00 AM');
+
+/* Outubro */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1020,10.20,'20201009 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1020,12.12,'20201013 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1023,19.50,'20201014 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2035,1021,22.60,'20201022 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2036,1022,23.30,'20201025 6:00:00 AM');
+
+/* Novembro */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1021,93.30,'20201109 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1022,103.30,'20201113 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1024,123.30,'20201114 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1023,223.30,'20201122 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1023,323.30,'20201125 6:00:00 AM');
+
+/* Dezembro */
+
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2034,1023,423.30,'20201209 1:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2033,1024,523.30,'20201213 5:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2032,1021,723.30,'20201214 8:00:00 PM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2038,1022,923.30,'20201222 10:00:00 AM');
+INSERT INTO Lancamento(CategoriaId,ContaId,Valor,CreationDate) VALUES (2037,1020,1123.30,'20201225 6:00:00 AM');
+
